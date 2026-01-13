d0ac5bd4a1e92191af4dd3f20e1ad2bafd870930
goodking <wanghaoxinvv@gmail.com>
2026-01-13 19:49:05 +0800

    多访问路径@RequestMapping(value = {"/test1","/test01"})

--- SMDemo01/src/main/java/com/gk/controller/MyController.java
+++ SMDemo01/src/main/java/com/gk/controller/MyController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * mycontroller 控制器，控制类
  */
 @Controller//放入springmvc容器
+@RequestMapping("mvc1")
 public class MyController {
 
-    @RequestMapping("test")
+    @RequestMapping(value = {"/test1","/test01"})
     public String test1() {
         System.out.println("控制单元被访问！！");
         return "/first.jsp";
