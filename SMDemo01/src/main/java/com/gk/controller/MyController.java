924b237ac5cdb3b8a0f4f051f2eb2ee63015936b
goodking <wanghaoxinvv@gmail.com>
2026-01-13 19:10:31 +0800

    springmvc第一个项目测试

---
+++ SMDemo01/src/main/java/com/gk/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.gk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * mycontroller 控制器，控制类
+ */
+@Controller//放入springmvc容器
+public class MyController {
+
+    @RequestMapping("test")
+    public String test1() {
+        System.out.println("控制单元被访问！！");
+        return "/first.jsp";
+    }
+}
