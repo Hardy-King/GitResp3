de0c908cb4f6841970172486273ddc3c6e117a7a
goodking <wanghaoxinvv@gmail.com>
2026-01-13 21:45:50 +0800

    CONTROLLER2测试

---
+++ SMDemo01/src/main/java/com/gk/controller/MyController2.java
@@ -0,0 +1,18 @@
+package com.gk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * mycontroller 控制器，控制类
+ */
+@Controller//放入springmvc容器
+@RequestMapping("mvc2")
+public class MyController2 {
+
+    @RequestMapping(value = {"/test1","/test01"})
+    public String test1() {
+        System.out.println("控制单元被访问！！");
+        return "/first.jsp";
+    }
+}
